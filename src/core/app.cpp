401709bad9293e2b49860065c26468ddb191c643
wh <wh@huawei.com>
2024-06-09 11:14:32 +0800

    add debug logger in debug mode

--- src/core/app.cpp
+++ src/core/app.cpp
@@ -11,12 +11,17 @@ using namespace Moon;
 
 
 void App::init() {
-    // https://github.com/gabime/spdlog/wiki/3.-Custom-formatting
-    spdlog::set_pattern("%L %m-%d %T.%e %s:%# %v");
-    spdlog::flush_every(std::chrono::seconds(1));
     auto max_size = 50 * 1024 * 1024;  // 50MB
     auto max_files = 10;
     auto logger = spdlog::rotating_logger_mt(kAppName, "log/rotating.txt", max_size, max_files);
+    // https://github.com/gabime/spdlog/wiki/3.-Custom-formatting
+    logger->set_pattern("%L %m-%d %T.%e %s:%# %v");
+    logger->flush_on(spdlog::level::trace);
+#ifdef NDEBUG
+    logger->set_level(spdlog::level::info);
+#else
+    logger->set_level(spdlog::level::trace);
+#endif
     spdlog::set_default_logger(logger);
     SPDLOG_INFO(kAsciiLogo);
 }
